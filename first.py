01a3f2173e7a5f880050f0ff6839a113fc353e2d
Sergey <gorshkovsa1602@yandex.ru>
2020-11-20 17:00:53 +0300

    first work

--- first.py
+++ first.py
@@ -1,1 +1,35 @@
-print("My first git-repo")
+import sys
+from random import randint
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtGui import QPainter, QColor, QPixmap, QPen
+
+
+class Zadacha2(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('Ui.ui', self)
+        self.initUI()
+
+    def initUI(self):
+        self.for_krug.setPixmap(QPixmap(501, 361))
+        self.pushButton.clicked.connect(self.krug)
+
+    def krug(self):
+        x, y = [randint(10, 360) for i in range(2)]
+        w, h = [randint(10, 100) for i in range(2)]
+        painter = QPainter(self.for_krug.pixmap())
+        pen = QPen()
+        pen.setWidth(3)
+        pen.setColor(QColor(255, 255, 0))
+        painter.setPen(pen)
+        painter.drawEllipse(x, y, w, h)
+        painter.end()
+        self.update()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Zadacha2()
+    ex.show()
+    sys.exit(app.exec_())
